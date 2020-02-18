8be21dd322171e28039a8dce5265ae10043602be
yoh-ito <58355678+yoh-ito@users.noreply.github.com>
2020-02-18 19:29:39 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.12.0'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:yoh-ito/s84a108.git'
+set :repo_url,  'git@github.com:yoh-ito/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
