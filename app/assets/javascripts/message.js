7a227b68d34dc5a34383eea61f0309433a0b0362
yoh-ito <58355678+yoh-ito@users.noreply.github.com>
2020-02-20 19:52:11 +0900

    edit 2
    edited

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,36 +1,36 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
+  if ( message.image ) {
+    var html =
       `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <img src=${message.image} >
+      </div>`
+    return html;
+  } else {
+    var html =
       `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
            <p class="lower-message__content">
              ${message.content}
            </p>
@@ -38,37 +38,37 @@ $(function(){
        </div>`
      return html;
    };
-  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 7000);
- }
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
+  };
+
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
    url: url,
    type: "POST",
    data: formData,
    dataType: 'json',
    processData: false,
    contentType: false
- })
+   })
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});      
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 10);      
     $('form')[0].reset();
     $(".form__submit").prop('disabled', false);
   })
-  .fail(function() {
+  .fail(function() {  
     alert("メッセージ送信に失敗しました");
-　});
-})
+  });
+  })
 
-var reloadMessages = function() {
-  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-  last_message_id = $('.message:last').data("message-id");
-  $.ajax({
+    var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    console.log(last_message_id);
+    $.ajax({
     //ルーティングで設定した通りのURLを指定
     url: "api/messages",
     //ルーティングで設定した通りhttpメソッドをgetに指定
@@ -76,8 +76,8 @@ var reloadMessages = function() {
     dataType: 'json',
     //dataオプションでリクエストに値を含める
     data: {id: last_message_id}
-  })
-  .done(function(messages) {
+    })
+    .done(function(messages) {
     //追加するHTMLの入れ物を作る
     var insertHTML = '';
     //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
@@ -86,5 +86,9 @@ var reloadMessages = function() {
     });
     //メッセージが入ったHTMLに、入れ物ごと追加
     $('.messages').append(insertHTML);
-  });
-};
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/));
+    setInterval(reloadMessages, 7000)
+    
+});
