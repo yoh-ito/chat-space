a308de4742f04478f7bcbcddfad8b333a74a22e5
yoh-ito <58355678+yoh-ito@users.noreply.github.com>
2020-02-21 20:29:31 +0900

    edit5
    edited

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,23 +1,6 @@
 $(function(){ 
   function buildHTML(message){
   if ( message.image ) {
-    // var html =
-    //   `<div class="message" data-message-id=${message.id}>
-    //     <div class="upper-message">
-    //       <div class="upper-message__user-name">
-    //         ${message.user_name}
-    //       </div>
-    //       <div class="upper-message__date">
-    //         ${message.created_at}
-    //       </div>
-    //     </div>
-    //     <div class="lower-message">
-    //       <p class="lower-message__content">
-    //         ${message.content}
-    //       </p>
-    //     </div>
-    //     <img src=${message.image} >
-    //   </div>`
     var html = `<div class="message" data-message-id=${message.id}>
     <div class="message__upper-info__talker">
     ${message.user_name}
@@ -30,26 +13,9 @@ $(function(){
     ${message.content}
     </div>
     <img src=${message.image} ></img>`
-    
+
     return html;
   } else {
-    // var html =
-    //   `<div class="message" data-message-id=${message.id}>
-    //     <div class="upper-message">
-    //       <div class="upper-message__user-name">
-    //         ${message.user_name}
-    //       </div>
-    //       <div class="upper-message__date">
-    //         ${message.created_at}
-    //       </div>
-    //     </div>
-    //     <div class="lower-message">
-    //        <p class="lower-message__content">
-    //          ${message.content}
-    //        </p>
-    //      </div>
-    //    </div>`
-
     var html = `<div class="message" data-message-id=${message.id}>
     <div class="message__upper-info__talker">
     ${message.user_name}
